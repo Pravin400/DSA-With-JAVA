78a8fa1189fed5c73b65c33c7c5c593f7e9a06b1
max125-c <pravintambe5671@gmail.com>
2025-08-09 00:00:01 +0530

    Added Sub Array program to get sub arrays from array

---
+++ src/src/Arrays/Day003_Arrays/Array_SubArray.java
@@ -0,0 +1,14 @@
+package Arrays.Day003_Arrays;
+
+public class Array_SubArray {
+    public static void main(String[] args) {
+        int [] number = {2,3,4,5,6,7,8};
+
+    }
+     public static void subArray(int [] number){
+        for (int i =0; i < number.length;i++){
+            for (int j = i+1;j<number.length;j++){
+                            }
+        }
+    }
+}
