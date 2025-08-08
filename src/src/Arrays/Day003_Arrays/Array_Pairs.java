16b85e2255fcba5f56a70640ea8db2719eab05a3
max125-c <pravintambe5671@gmail.com>
2025-08-08 23:40:42 +0530

    Added Array_Pairs program to generate and display all unique element pairs with pair count

--- src/src/Arrays/Day003_Arrays/Array_Pairs.java
+++ src/src/Arrays/Day003_Arrays/Array_Pairs.java
@@ -5,6 +5,7 @@ public class Array_Pairs {
     public static void main(String[] args) {
 
     int [] number = {2,3,4,5};
+
     disp(number);
         System.out.println("\nAll Unique Pairs:");
 
@@ -16,12 +17,16 @@ public class Array_Pairs {
         }
     }
     public static void pair(int [] number){
+        int count = 0;
         for(int i = 0;i<number.length;i++){
             for (int j = i+1;j <number.length;j++){
                 System.out.print("(" + number[i] + "," + number[j] + ") ");
+                count++;
 
             }
+
             System.out.println();
         }
+        System.out.println("The pairs are: " + count);
     }
 }
