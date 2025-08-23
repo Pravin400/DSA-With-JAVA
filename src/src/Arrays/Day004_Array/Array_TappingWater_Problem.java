9e527333220b12caba5ddd0d7f0c594f7fa8fd7b
max125-c <pravintambe5671@gmail.com>
2025-08-23 23:45:01 +0530

    Tapping Water Peoblem Solved here

--- src/src/Arrays/Day004_Array/Array_TappingWater_Problem.java
+++ src/src/Arrays/Day004_Array/Array_TappingWater_Problem.java
@@ -3,34 +3,39 @@ package Arrays.Day004_Array;
 public class Array_TappingWater_Problem {
 
     public static int tappedWater(int height[]) {
-    //here calculate teh left max bondary of array
+        // Step 1: Calculate left max boundary
         int leftMax[] = new int[height.length];
         leftMax[0] = height[0];
-        for(int i = 1;i<height.length;i++){
+        for(int i = 1; i < height.length; i++){
             leftMax[i] = Math.max(height[i], leftMax[i-1]);
         }
-    //here calculate the right max boundary array
+
+        // Step 2: Calculate right max boundary
         int rightMax[] = new int[height.length];
         rightMax[height.length-1] = height[height.length-1];
-        for(int i=height.length-2;i>=0;i--){
+        for(int i = height.length - 2; i >= 0; i--){
             rightMax[i] = Math.max(height[i], rightMax[i+1]);
         }
 
-        //loop for calculating tapped water
-        int tappedWater = 0;//initially tepped water is 0
-        for(int i = 0;i<height.length;i++){
-             //waterlevel = min(leftmax bounnd,rightmax bound)
-            int waterLevel = Math.min(leftMax[i],rightMax[i]);
-            //trapped water = wwaterlevel - height[i]
-            tappedWater += waterLevel -height[i];
+        // Step 3: Calculate trapped water
+        int tappedWater = 0;
+        for(int i = 0; i < height.length; i++){
+            int waterLevel = Math.min(leftMax[i], rightMax[i]);
+            tappedWater += waterLevel - height[i];
         }
         return tappedWater;
-
     }
 
     public static void main(String[] args) {
-        int height [] = { 4,2,0,6,3,2,5};
-        System.out.println(tappedWater(height));
+        int height [] = { 4,2,0,6,3,2,5 };
+        //           |
+//                   |           |
+//      |            |           |
+//      |            |   |       |
+//      |   |        |   |   |   |
+//      |   |        |   |   |   |
+//     -------------------------
 
+        System.out.println(tappedWater(height)); // Output: 11
     }
 }
