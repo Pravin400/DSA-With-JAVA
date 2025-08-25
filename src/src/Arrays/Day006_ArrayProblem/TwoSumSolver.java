312944bf56a51380c6667052c65e221972fd6fe4
max125-c <pravintambe5671@gmail.com>
2025-08-25 21:11:21 +0530

    Fix: correct Two Sum brute-force logic by checking pairs directly instead of cumulative sum

--- src/src/Arrays/Day006_ArrayProblem/TwoSumSolver.java
+++ src/src/Arrays/Day006_ArrayProblem/TwoSumSolver.java
@@ -8,12 +8,12 @@ public class TwoSumSolver {
     }
     public static void sum(int[] num){
         int target = 9;
-        int sum = 0;
+
         for(int i=0;i<num.length;i++){
-            sum = num[i];
+
             for(int j=i+1;j<num.length;j++){
-                sum+=num[j];
-                if(sum==target){
+
+                if(num[i]+num[j]==target){
 
                     System.out.println("{"+i+","+j+"}");
                     return;
