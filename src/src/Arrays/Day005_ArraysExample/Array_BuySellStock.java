42d0d57c061bc575e886969b1239563fba793cd5
max125-c <pravintambe5671@gmail.com>
2025-08-25 19:45:30 +0530

    enchanced buy and sell stock to get more profit

--- src/src/Arrays/Day005_ArraysExample/Array_BuySellStock.java
+++ src/src/Arrays/Day005_ArraysExample/Array_BuySellStock.java
@@ -1,27 +1,29 @@
 package Arrays.Day005_ArraysExample;
 
 public class Array_BuySellStock {
-    public static int byuAndSellStocks(int[] stockPrices){
-         int buyPrice = Integer.MAX_VALUE;
-         int maxProfit = 0;
 
-         for(int i = 0; i < stockPrices.length; i++){
-             if(stockPrices[i] > buyPrice){
-                 int Profit = stockPrices[i] - buyPrice;
-                 System.out.println("sell Price: " + stockPrices[i] + " Profit: " + Profit);
-                 maxProfit = Math.max(maxProfit, Profit);
-             }
-             else {
-                 buyPrice = stockPrices[i];
-                 System.out.println("Buy Price is " + buyPrice);
-             }
+    // Function to calculate maximum profit
+    public static int buyAndSellStocks(int[] stockPrices) {
+        int buyPrice = Integer.MAX_VALUE; // Initially set buy price to infinity
+        int maxProfit = 0;
 
-         }
-         return maxProfit;
+        for (int i = 0; i < stockPrices.length; i++) {
+            // If we get a better selling opportunity
+            if (stockPrices[i] > buyPrice) {
+                int profit = stockPrices[i] - buyPrice;
+                System.out.println("Sell Price: " + stockPrices[i] + " | Profit: " + profit);
+                maxProfit = Math.max(maxProfit, profit);
+            } else {
+                // Update buy price if stock is cheaper
+                buyPrice = stockPrices[i];
+                System.out.println("Buy Price updated to: " + buyPrice);
+            }
+        }
+        return maxProfit;
     }
 
     public static void main(String[] args) {
-        int[]  stockPrice= {7,1,5,3,6,4};
-        System.out.println(byuAndSellStocks(stockPrice));
+        int[] stockPrices = {7, 1, 5, 3, 6, 4};
+        System.out.println("Maximum Profit: " + buyAndSellStocks(stockPrices));
     }
 }
