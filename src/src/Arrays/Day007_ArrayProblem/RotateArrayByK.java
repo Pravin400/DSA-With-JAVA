0a033d59927a017bd4dfa05e1b3846401e389d45
max125-c <pravintambe5671@gmail.com>
2025-08-26 23:53:52 +0530

    Rotate the array At Index k

---
+++ src/src/Arrays/Day007_ArrayProblem/RotateArrayByK.java
@@ -0,0 +1,13 @@
+package Arrays.Day007_ArrayProblem;
+
+public class RotateArrayByK {
+    public static void main(String[] args) {
+        int [] num = {1,2,3,4,5,6,7,8,9,10};
+
+        Rotate(num);
+    }
+
+    private static void Rotate(int[] num) {
+
+    }
+}
