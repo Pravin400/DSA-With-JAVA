c8e70733b4215eb673c752e3d7d0f91e2a91b3a3
max125-c <pravintambe5671@gmail.com>
2025-08-26 23:58:34 +0530

    Rotate the array At Index done

--- src/src/Arrays/Day007_ArrayProblem/RotateArrayByK.java
+++ src/src/Arrays/Day007_ArrayProblem/RotateArrayByK.java
@@ -1,13 +1,34 @@
 package Arrays.Day007_ArrayProblem;
 
+import java.util.Arrays;
+
 public class RotateArrayByK {
     public static void main(String[] args) {
         int [] num = {1,2,3,4,5,6,7,8,9,10};
+        int k = 3;
+        Rotate(num,k);
+        System.out.println("Rotated Array: " + Arrays.toString(num));
 
-        Rotate(num);
     }
 
-    private static void Rotate(int[] num) {
+    private static void Rotate(int[] num,int k) {
+        int n = num.length;
+        k = k % n;
 
+        reverse(num,0,n-1);
+        reverse(num,0,k-1);
+        reverse(num,k,n-1);
+
+
+
+    }
+    private static void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
     }
 }
