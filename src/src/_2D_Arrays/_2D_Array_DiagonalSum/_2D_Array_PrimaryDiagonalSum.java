89863a39165427ef1bdfaadd54c10f69f6f0c617
max125-c <pravintambe5671@gmail.com>
2025-09-02 20:53:58 +0530

    very efficiently and logically solve the sum of the primary diagonal with less time complexity

---
+++ src/src/_2D_Arrays/_2D_Array_DiagonalSum/_2D_Array_PrimaryDiagonalSum.java
@@ -0,0 +1,27 @@
+package _2D_Arrays._2D_Array_DiagonalSum;
+
+import java.util.Arrays;
+
+public class _2D_Array_PrimaryDiagonalSum {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+        };
+        diagonalSum(matrix);
+    }
+
+    public static void diagonalSum(int[][] matrix) {
+        int diagonalSum = 0;
+        int[] diagonalElements = new int[matrix.length];
+
+        for(int i = 0; i < matrix.length; i++) {
+            diagonalElements[i] = matrix[i][i];
+            diagonalSum += matrix[i][i];
+        }
+
+        System.out.println("Primary Diagonal Elements: " + Arrays.toString(diagonalElements));
+        System.out.println("Primary Diagonal Sum: " + diagonalSum);
+    }
+}
