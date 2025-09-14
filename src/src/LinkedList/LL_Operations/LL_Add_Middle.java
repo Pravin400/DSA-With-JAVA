c6ad2875a65a06de5c58d991506c173645a7cc53
max125-c <pravintambe5671@gmail.com>
2025-09-14 22:44:21 +0530

    Addming the element in the middle of the linked list

---
+++ src/src/LinkedList/LL_Operations/LL_Add_Middle.java
@@ -0,0 +1,71 @@
+package LinkedList.LL_Operations;
+
+public class LL_Add_Middle {
+    public static class Node{
+        int data;
+        Node next;
+        public Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+
+    public static void addLast(int data){
+        Node newNode=new Node(data);
+        if(head==null){
+            head = tail=newNode;
+            return;
+        }
+        tail.next=newNode;
+        tail=newNode;
+    }
+    public static void addFirst(int data){
+        Node newNode=new Node(data);
+        if(head==null){
+            head = tail=newNode;
+            return;
+        }
+        newNode.next=head;
+        head=newNode;
+    }
+
+    public static void print(){
+        Node temp=head;
+        while(temp!=null){
+            System.out.print(temp.data+" -> ");
+            temp=temp.next;
+        }
+        System.out.println("Null");
+    }
+    public static void addMiddle(int data,int idx){
+        if(idx==0){
+            addFirst(data);
+            return;
+        }
+        Node newNode=new Node(data);
+        Node temp=head;
+        System.out.println(head.data);
+        int i=0;
+        while (i < idx-1){
+            temp=temp.next;
+            i++;
+
+        }
+        newNode.next=temp.next;
+        System.out.println(newNode.next.data);
+        temp.next=newNode;
+        System.out.println(temp.next.data);
+    }
+
+    public static void main(String[] args) {
+        addLast(10);
+        addLast(20);
+        addLast(30);
+        addLast(50);
+        print();
+        addMiddle(40,0);
+        print();
+    }
+}
