418d552b02740794128bd58c44ccb02e2352c515
Pravin400 <pravintambe5671@gmail.com>
2025-10-05 19:53:37 +0530

    Code of the DLL Reverse

---
+++ src/src/LinkedList/Doubly_LL/Doubly_LL_Reverse.java
@@ -0,0 +1,112 @@
+package LinkedList.Doubly_LL;
+
+public class Doubly_LL_Reverse {
+    public class Node {
+        int data;
+        Node next;
+        Node prev;
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    //add
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+
+        size++;
+        if(head==null){
+            head=tail=newNode;
+            return;
+        }
+        newNode.next = head;
+        head.prev = newNode;
+        head = newNode;
+
+        }
+
+    //Last
+
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+
+        size++;
+        if(head==null){
+            head=tail=newNode;
+            return;
+        }
+        tail.next = newNode;
+        newNode.prev = tail;
+        tail = newNode;
+
+    }
+
+    //Reverse
+
+    public void reverse(){
+        Node curr = head;
+        Node prev = null;
+        Node next;
+        while(curr!=null){
+            next = curr.next;
+            curr.next = prev;
+            curr.prev = next;
+
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+    }
+
+
+    //print
+
+    public void print() {
+        Node temp = head;
+        while(temp!=null){
+            System.out.print(temp.data+"<->");
+            temp = temp.next;
+        }
+        System.out.print("null  ");
+    }
+
+    //remove fast
+
+    public int removeLast() {
+        if(head==null){
+            System.out.println("the list is empty");
+            return -1;
+        }
+        if(size == 1){
+            int val = tail.data;
+            head = tail = null;
+            size --;
+            return val;
+        }
+        int val = tail.data;
+        tail = tail.prev;
+        tail.next = null;
+        size--;
+        return val;
+    }
+
+    public static void main(String[] args) {
+        Doubly_LL_Reverse dll = new Doubly_LL_Reverse();
+        dll.addFirst(3 );
+        dll.addFirst(2);
+        dll.addFirst(1);
+        dll.addFirst(0);
+        dll.print();
+        System.out.println("\nsize="+size);
+        dll.reverse();
+        dll.print();
+        System.out.println("\nsize="+size);
+
+
+    }
+}
